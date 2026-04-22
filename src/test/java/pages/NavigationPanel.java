8fd61c7a3530c86604f442d35b760f0d6e17a8b4
gloom <aleksey.koryagin86@gmail.com>
2026-04-22 15:03:04 +0300

    Подправил локаторы в NavigationPanel.

--- src/test/java/pages/NavigationPanel.java
+++ src/test/java/pages/NavigationPanel.java
@@ -4,14 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utils.PropertyReader;
 
+import static pages.BasePage.DATA_TEST_CSS_PATTERN;
+
 public class NavigationPanel {
     public static final String COMPANY_MAIN_PAGE_URL = PropertyReader.getProperty("automation_in_java.companyMainPageUrl");
 
     private final By burgerMenu = By.id("react-burger-menu-btn");
-    private final By logoutLink = By.cssSelector(BasePage.DATA_TEST_CSS_PATTERN.formatted("logout-sidebar-link"));
-    private final By cartLink = By.cssSelector(BasePage.DATA_TEST_CSS_PATTERN.formatted("shopping-cart-link"));
-    private final By countThingsInCartBadge = By.cssSelector(BasePage.DATA_TEST_CSS_PATTERN.formatted("shopping-cart-badge"));
-    private final By aboutLink = By.cssSelector(BasePage.DATA_TEST_CSS_PATTERN.formatted("about-sidebar-link"));
+    private final By logoutLink = By.cssSelector(DATA_TEST_CSS_PATTERN.formatted("logout-sidebar-link"));
+    private final By cartLink = By.cssSelector(DATA_TEST_CSS_PATTERN.formatted("shopping-cart-link"));
+    private final By countThingsInCartBadge = By.cssSelector(DATA_TEST_CSS_PATTERN.formatted("shopping-cart-badge"));
+    private final By aboutLink = By.cssSelector(DATA_TEST_CSS_PATTERN.formatted("about-sidebar-link"));
 
     WebDriver driver;
 
